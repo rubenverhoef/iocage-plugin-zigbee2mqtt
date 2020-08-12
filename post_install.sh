b9dd7141090350bb7eb8ca415745f0c03296bf0f
Ruben Verhoef <ruben_verhoef@hotmail.com>
2020-08-12 21:14:10 +0200

    first installation try

--- post_install.sh
+++ post_install.sh
@@ -1,1 +1,12 @@
 #!/bin/sh
+
+
+# 2. Installing #
+# Clone Zigbee2MQTT repository
+git clone https://github.com/Koenkk/zigbee2mqtt.git /home/root/zigbee2mqtt
+
+# Make python command accessible
+ln -s /usr/local/bin/python3.7 /usr/local/bin/python
+
+# Install dependencies
+cd /home/root/zigbee2mqtt && npm ci
