a67d54966ee26ddbba59a96834769b7e0d080c97
rubenverhoef <ruben_verhoef@hotmail.com>
2021-09-19 17:17:57 +0200

    python version is now 3.8

--- post_install.sh
+++ post_install.sh
@@ -28,7 +28,7 @@ configure_config()
 git clone https://github.com/Koenkk/zigbee2mqtt.git /home/root/zigbee2mqtt
 
 # Make python command accessible
-ln -s /usr/local/bin/python3.7 /usr/local/bin/python
+ln -s /usr/local/bin/python3.8 /usr/local/bin/python
 
 # Install dependencies
 cd /home/root/zigbee2mqtt && npm ci
